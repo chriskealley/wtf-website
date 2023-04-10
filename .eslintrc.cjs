55dc7bf1581e19fb0499c50e750968ccaca68dfb
Chris Kealley <chriskealley@gmail.com>
2023-04-10 19:16:32 +0800

    Added UnoCSS, updated eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,50 @@
 module.exports = {
-  extends: ["plugin:astro/recommended", "prettier"],
+  env: {
+    node: true,
+    es2022: true,
+    browser: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:astro/recommended",
+    "@unocss",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
   overrides: [
     {
       files: ["*.astro"],
       parser: "astro-eslint-parser",
-
       parserOptions: {
         parser: "@typescript-eslint/parser",
         extraFileExtensions: [".astro"],
       },
       rules: {},
+    },
+    {
+      files: ["*.ts"],
+      parser: "@typescript-eslint/parser",
+      extends: ["plugin:@typescript-eslint/recommended"],
+      rules: {
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
+        ],
+        "@typescript-eslint/no-non-null-assertion": "off",
+      },
+    },
+    {
+      files: ["*.d.ts"],
+      rules: {
+        "@typescript-eslint/triple-slash-reference": "off",
+      },
+    },
+    {
+      files: ["**/*.astro/*.js", "*.astro/*.js"],
+      parser: "@typescript-eslint/parser",
     },
   ],
 };
